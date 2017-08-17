9c26e627dd8c5c9a5f7618fe19b70454e68be101
unknown <adimunteanu13@gmail.com>
2017-08-17 11:53:10 +0200

    Youtube search app (without Redux)

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
     render() {
-        return <input onChange={ event => console.log(event.target.value) } />;
+        return (
+            <div className="search-bar">
+                <input 
+                    value={this.state.term}
+                    onChange={ event => this.onInputChange(event.target.value) } />
+            </div>
+        );
     }
 
-    // onInputChange(event) {
-    //     console.log(event.target.value);
-    // }
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
 }
 
 export default SearchBar;
